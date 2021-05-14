38c32af60b3cd3f41934e783ec2b590c2173d493
rohithv07 <rohithv63@gmail.com>
2021-05-14 16:45:53 +0530

    Dockerfile updated, provided working directory, minimised cache busting and rebuild

--- NodeJsWebApp/index.js
+++ NodeJsWebApp/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send("Hi there");
+    res.send("Hi there rohith mahn");
 });
 
 app.listen(8080, () => {
