2e1e159f01113f60995abc11e1b7730cba884968
rohithv07 <rohithv63@gmail.com>
2021-05-24 19:24:59 +0530

    Multiple Containers Test

---
+++ Complex/client/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+         <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+         </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ Complex/client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default() => {
+    return(
+        <div>
+            In Some other pather
+            <Link to="/">Go Back Home</Link>
+        </div>
+    );
+};
